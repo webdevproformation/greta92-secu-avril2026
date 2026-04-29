8b376dd65f957bfc7f6f7968b6d9b52738caef8d
Malik H <malik.h@webdevpro.net>
2026-04-29 10:33:02 +0200

    exemple 1 CSP

--- jour07/02-exemple.php
+++ jour07/02-exemple.php
@@ -1,5 +1,14 @@
 <?php 
 
+// Navigateur SI tu as dans le html du javascript NE l'execute pas 
 header("Content-Security-Policy: default-src 'none';");
 
 echo "bonjour" ; 
+?>
+<script>alert("virus")</script>
+
+// cd jour07
+// php -S localhost:1235
+// lancer un navigateur web 
+// http://localhost:1235/02-exemple.php
+// regarder l'onglet reseau 
