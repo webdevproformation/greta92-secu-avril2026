04d88d2035bb1cb3e4308d5f7fe4947fd286ad70
Malik H <malik.h@webdevpro.net>
2026-05-05 12:04:41 +0200

    correction

---
+++ jour09/03-exo.php
@@ -0,0 +1,14 @@
+<?php 
+
+$a = 30 ;
+$b = [30,40, 20, 10, 60];
+$c = false ;
+$d = "j'ai réussi !!" ; 
+
+// opération 
+// string => concaténation
+// chiffre => addition soustraction multiplication division module puissance
+// boolean => opération boolean
+// tableau parcours de tableau
+// fonction
+// POO 
