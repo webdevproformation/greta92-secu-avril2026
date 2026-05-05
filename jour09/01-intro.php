b48119a91fbe137f5947c51ede7d2e02763144a5
Malik H <malik.h@webdevpro.net>
2026-05-05 10:42:42 +0200

    premier script

---
+++ jour09/01-intro.php
@@ -0,0 +1,28 @@
+<?php  // lorsque je veux écrire du PHP
+       // ouvrir une balise spéciale
+echo "bonjour" ;
+
+// echo => mot clé du langage PHP afficher à l'écran
+// espace facultatif
+// " guillemet ouvrante
+// du français le mot bonjour
+// " guillemet fermante
+// espace facultatif 
+// ; OBLIGATOIRE il met fin à l'instruction (statement)
+
+// j'ai fini d'écrire du PHP 
+
+echo "<p>salut</p>"; 
+
+// Attention PHP est langage SERVEUR 
+// pour qu'il fonctionne correctement il faut qu'il soit exécuté PAR le serveur (Apache)
+
+
+// Dans le langage PHP, il y a désormais un serveur interne disponible
+// il suffit juste de le démarrer
+// lancer un terminal
+// cd jour09
+// php -S localhost:1234 
+// (version minituature de VirtualBox)
+// dans votre navigateur http://localhost:1234/01-intro.php
+?>   
