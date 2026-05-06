d2d6b3ca2ec0b71b1dd467e5eab4dfa96e85e844
Malik H <malik.h@webdevpro.net>
2026-05-06 16:44:07 +0200

    ajustements

--- jour10/11-exo.php
+++ jour10/11-exo.php
@@ -7,9 +7,11 @@ function calculMoyenne( $tableau )
     for($i = 0 ; $i < count($tableau) ; $i++){
         $total += $tableau[$i];
     }
-    echo $total / count($tableau);
+    echo $total / count($tableau) . "<br>";
 }
 
 
  calculMoyenne( [ 1 ,2 ,3 , 4  ] );
  calculMoyenne( [ 12 , 30 ,25.3  ] );
+
+// dans mon navigateur http://localhost:1234/11-exo.php
