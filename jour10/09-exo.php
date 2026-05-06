1f9b96ab40fec05e7f18bf396c0d73fe0d3f959a
Malik H <malik.h@webdevpro.net>
2026-05-06 16:11:01 +0200

    correction

---
+++ jour10/09-exo.php
@@ -0,0 +1,10 @@
+<?php 
+
+function aireCercle()
+{
+    $resultat = 3.14 * 20 ** 2 ;
+    echo "l'aire d'un cercle de rayon 20 cm a une aire de $resultat cm²";
+}
+
+aireCercle();
+// dans mon navigateur http://localhost:1234/09-exo.php
